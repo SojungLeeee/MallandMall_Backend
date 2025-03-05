3b4447074765b5b8d3d5ebd786bd19dff52aa662
Sojung Lee <dlthwjd1118@gmail.com>
2025-03-05 11:19:21 +0900

    feat : backend signup+login
    signup+login 까지 된 백엔드

---
+++ src/main/java/com/exam/security/JwtTokenResponse.java
@@ -0,0 +1,18 @@
+package com.exam.security;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class JwtTokenResponse {
+	
+//	 - 용도: token 과 로그인한 userid 저장
+//     필요시 추가 정보 저장 가능함.
+	
+	String token;
+	String userid;
+
+}

732d44d7ad3ddb11b435aaa0fd6933bcf43e12ad
Sojung Lee <dlthwjd1118@gmail.com>
2025-03-05 13:36:27 +0900

    feat : member entity - createDate 추가
    member entity 에서 createDate 컬럼을 추가했습니다.

--- src/main/java/com/exam/member/MemberDTO.java
+++ src/main/java/com/exam/member/MemberDTO.java
@@ -1,5 +1,10 @@
 package com.exam.member;
 
+import java.time.LocalDate;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,13 +19,17 @@ import lombok.ToString;
 @ToString
 @Builder
 public class MemberDTO {
-    String userid;  // 아이디 (primary key)
-    String passwd;  // 비밀번호
-    String username;  // 사용자 이름
-    String post;  // 주소
-    String addr1;  // 주소1
-    String addr2;  // 주소2
-    String phoneNumber;  // 전화번호
-    String email;  // 이메일
-    String role = "USER";  // 역할, 기본값 'USER'
+	String userid;  // 아이디 (primary key)
+	String passwd;  // 비밀번호
+	String username;  // 사용자 이름
+	String post;  // 주소
+	String addr1;  // 주소1
+	String addr2;  // 주소2
+	String phoneNumber;  // 전화번호
+	String email;  // 이메일
+	String role = "USER";  // 역할, 기본값 'USER'
+
+	@CreationTimestamp
+	@Column(updatable = false) //저장할때만 자동저장O 수정할때는 저장 X 을위한것
+	LocalDate createDate; //저장할때만 자동저장O 수정할때는 저장 X
 }

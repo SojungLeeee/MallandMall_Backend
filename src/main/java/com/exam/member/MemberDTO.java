e947cb28310325eee3fffe63f1359e4d6b139316
Sojung Lee <dlthwjd1118@gmail.com>
2025-03-07 17:37:39 +0900

    feat : signup 주소, 유효성X

--- src/main/java/com/exam/member/MemberDTO.java
+++ src/main/java/com/exam/member/MemberDTO.java
@@ -1,5 +1,12 @@
 package com.exam.member;
 
+import java.time.LocalDate;
+
+import org.apache.ibatis.type.Alias;
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,14 +20,28 @@ import lombok.ToString;
 @Setter
 @ToString
 @Builder
+@Alias("MemberDTO")
 public class MemberDTO {
-    String userid;  // 아이디 (primary key)
-    String passwd;  // 비밀번호
-    String username;  // 사용자 이름
-    String post;  // 주소
-    String addr1;  // 주소1
-    String addr2;  // 주소2
-    String phoneNumber;  // 전화번호
-    String email;  // 이메일
-    String role = "USER";  // 역할, 기본값 'USER'
+
+	@NotBlank(message = "아이디 입력 필수")
+	String userid;  // 아이디 (primary key)
+
+	@NotBlank(message = "비밀번호 입력 필수")
+	String passwd;  // 비밀번호
+
+	@NotBlank(message = "사용자명 입력 필수")
+	String username;  // 사용자 이름
+
+	String post;  // 주소
+	String addr1;  // 주소1
+	String addr2;  // 주소2
+	String phoneNumber;  // 전화번호
+
+	String email;  // 이메일
+
+	String role = "USER";  // 역할, 기본값 'USER'
+
+	@CreationTimestamp
+	@Column(updatable = false) //저장할때만 자동저장O 수정할때는 저장 X 을위한것
+	LocalDate createDate; //저장할때만 자동저장O 수정할때는 저장 X
 }

db93a555423236ced5f0feb6575d2a9da7cb5103
Sojung Lee <dlthwjd1118@gmail.com>
2025-03-11 13:28:45 +0900

    feat:유효성 체크까지 완료

---
+++ src/main/java/com/exam/mypage/MypageDTO.java
@@ -0,0 +1,27 @@
+package com.exam.mypage;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class MypageDTO {
+
+	private String userid;
+	private String username;  // 사용자 이름
+	private String passwd;
+	private String post;  // 주소
+	private String addr1;  // 주소1
+	private String addr2;  // 주소2
+	private String phoneNumber;  // 전화번호
+	private String email;  // 이메일
+	private String role;
+}
---
+++ src/main/java/com/exam/mypage/MypageServiceImpl.java
@@ -0,0 +1,65 @@
+package com.exam.mypage;
+
+import org.springframework.stereotype.Service;
+
+import com.exam.user.User;
+
+@Service
+public class MypageServiceImpl implements MypageService {
+
+	private final MypageRepository mypageRepository;
+
+	public MypageServiceImpl(MypageRepository mypageRepository) {
+		this.mypageRepository = mypageRepository;
+	}
+
+	@Override
+	public MypageDTO getMypage(String userid) {
+
+		User user = mypageRepository.findByUserid(userid).orElse(null);
+		if (user == null) {
+			return null;
+		}
+		return convertToDTO(user);
+	}
+
+	@Override
+	public void updateMypage(String userid, MypageDTO dto) {
+		User user = mypageRepository.findByUserid(userid).orElse(null);
+		if (user == null) {
+			throw new IllegalArgumentException("해당 사용자가 존재하지 않습니다.");
+		}
+
+		user.setUsername(dto.getUsername());
+		user.setPhoneNumber(dto.getPhoneNumber());
+		user.setEmail(dto.getEmail());
+		user.setPost(dto.getPost());
+		user.setAddr1(dto.getAddr1());
+		user.setAddr2(dto.getAddr2());
+
+		mypageRepository.save(user);
+	}
+
+	@Override
+	public void deleteMypage(String userid) {
+		User user = mypageRepository.findByUserid(userid).orElse(null);
+		if (user == null) {
+			throw new IllegalArgumentException("해당 사용자가 존재하지 않습니다.");
+		}
+
+		mypageRepository.deleteById(userid);
+	}
+
+	private MypageDTO convertToDTO(User user) {
+		return MypageDTO.builder()
+			.userid(user.getUserid())
+			.username(user.getUsername())
+			.post(user.getPost())
+			.addr1(user.getAddr1())
+			.addr2(user.getAddr2())
+			.phoneNumber(user.getPhoneNumber())
+			.email(user.getEmail())
+			.role(user.getRole())
+			.build();
+	}
+}

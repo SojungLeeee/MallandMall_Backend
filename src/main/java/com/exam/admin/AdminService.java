4de82712fda8b093068ddf97853048f9d07f187f
Sojung Lee <dlthwjd1118@gmail.com>
2025-03-17 14:48:47 +0900

    feat : admin 폴더 추가

---
+++ src/main/java/com/exam/admin/AdminService.java
@@ -0,0 +1,4 @@
+package com.exam.admin;
+
+public interface AdminService {
+}
---
+++ src/main/java/com/exam/admin/AdminServiceImpl.java
@@ -0,0 +1,7 @@
+package com.exam.admin;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+}

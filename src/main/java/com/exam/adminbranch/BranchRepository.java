68e7b2e3b33f0ba0023971ca001569bcaa01d870
GeonHo Song <songgun1234@gmail.com>
2025-03-20 09:11:14 +0900

    feat: branch event 완성
    완성

--- src/main/java/com/exam/adminbranch/BranchRepository.java
+++ src/main/java/com/exam/adminbranch/BranchRepository.java
@@ -5,7 +5,5 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BranchRepository extends JpaRepository<Branch, String> {
-	//지점 이름 통해서 조회
-	Optional<Branch> findByBranchName(String branchName);
-
+	//JpaRepository에서 제공하는 CRUD 상속받아 사용
 }
